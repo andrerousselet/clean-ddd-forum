b45ecbb911d2ec639dceabb15c9ee3664c490ed7
Andre Rousselet <andre098@hotmail.com>
2023-10-16 17:48:46 -0300

    feat(domain-entities): add static method create() that acts like constructor

--- src/domain/entities/tutor.ts
+++ src/domain/entities/tutor.ts
@@ -1,7 +1,16 @@
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 
 interface TutorProps {
   name: string;
 }
 
-export class Tutor extends Entity<TutorProps> {}
+export class Tutor extends Entity<TutorProps> {
+  static create(
+    props: TutorProps,
+    id?: UniqueEntityID,
+  ) {
+    const tutor = new Tutor(props, id);
+    return tutor;
+  }
+}
